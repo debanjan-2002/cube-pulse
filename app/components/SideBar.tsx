754239d86afd1ba3e171cc04dc269c7ebac20a59
Debanjan Poddar <debanjan2602@gmail.com>
2024-03-07 23:19:36 +0530

    Changed styles

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -3,7 +3,7 @@ import Session from "./Session";
 
 const SideBar = () => {
     return (
-        <div className="p-4 space-y-4 w-fit h-full flex flex-col">
+        <div className="p-6 space-y-4 w-fit h-full flex flex-col border border-r-slate-800">
             <Session />
             <CurrentTimes />
         </div>
