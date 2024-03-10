4249cff4bcc6a7057693366e142b2a6fea090fee
Debanjan Poddar <debanjan2602@gmail.com>
2024-03-10 18:56:11 +0530

    Added analytics header component

---
+++ app/components/AnalyticsHeader.tsx
@@ -0,0 +1,9 @@
+const AnalyticsHeader = () => {
+    return (
+        <div className="p-6 text-4xl text-center whitespace-pre-wrap leading-tight">
+            Analytics
+        </div>
+    );
+};
+
+export default AnalyticsHeader;
--- app/timer/[sessionId]/analytics/page.tsx
+++ app/timer/[sessionId]/analytics/page.tsx
@@ -1,7 +1,12 @@
+import AnalyticsHeader from "@/app/components/AnalyticsHeader";
 import React from "react";
 
 const page = () => {
-    return <div>page</div>;
+    return (
+        <div>
+            <AnalyticsHeader />
+        </div>
+    );
 };
 
 export default page;
