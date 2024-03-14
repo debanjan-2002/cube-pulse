9d3c1ccc9256b18e2f790caeb164c65d0f45513d
Debanjan Poddar <debanjan2602@gmail.com>
2024-03-14 15:23:05 +0530

    Added line graph to show variation of session times w.r.t average time

--- app/components/Analytics/Charts/LineChart.tsx
+++ app/components/Analytics/Charts/LineChart.tsx
@@ -13,10 +13,10 @@ import {
     defaults
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+import { useTimer } from "@/app/hooks/useTimer";
 
 defaults.maintainAspectRatio = false;
 defaults.responsive = true;
-console.log(defaults);
 
 ChartJS.register(
     CategoryScale,
@@ -29,19 +29,44 @@ ChartJS.register(
 );
 
 export const options = {
-    // plugins: {
-    //     legend: {
-    //         position: "top" as const
-    //     },
-    //     title: {
-    //         display: true,
-    //         text: "Chart.js Line Chart"
-    //     }
-    // }
+    plugins: {
+        legend: {
+            // position: "top" as const,
+            display: false
+            // title: {
+            //     display: true,
+            //     font: {
+            //         size: 24
+            //     }
+            // },
+            // labels: {
+            //     font: {
+            //         size: 16
+            //     }
+            // }
+        },
+        title: {
+            display: true,
+            text: "Variation of Session times w.r.t to Average time",
+            font: {
+                size: 16
+            }
+        }
+    },
+
     scales: {
         x: {
             ticks: {
                 display: false
+            }
+        },
+        y: {
+            title: {
+                text: "Times (in sec.)",
+                display: true
+            },
+            ticks: {
+                stepSize: 0.5
             }
         }
     },
@@ -52,20 +77,34 @@ export const options = {
     }
 };
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+export default function LineChart() {
+    const { getSessionTimes } = useTimer();
+    const sessionTimes = getSessionTimes();
 
-export const data = {
-    labels,
-    datasets: [
-        {
-            label: "Dataset 1",
-            data: labels.map(() => Math.floor(Math.random() * 10)),
-            borderColor: "rgb(255, 99, 132)",
-            backgroundColor: "rgba(255, 99, 132, 0.5)"
-        }
-    ]
-};
+    const labels = sessionTimes.map((time, i) => i + 1);
+    const sum = sessionTimes.reduce((acc, time) => acc + parseFloat(time), 0);
+    const avg = sum / sessionTimes.length;
+    sessionTimes.reverse();
 
-export default function App() {
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: "Times of session",
+                data: sessionTimes.map(time => time),
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)"
+            },
+            {
+                label: "Average time of session",
+                data: sessionTimes.map(() => avg.toFixed(2)),
+                borderColor: "rgb(53, 162, 235)",
+                backgroundColor: "rgba(53, 162, 235, 0.5)",
+                pointRadius: 0,
+                borderDash: [5, 5]
+            }
+        ]
+    };
+
     return <Line data={data} options={options} />;
 }
