122056307410a7d1625027690ad4b523dffc0c44
Debanjan Poddar <debanjan2602@gmail.com>
2024-03-21 22:13:02 +0530

    Added custom colors

---
+++ app/constants/colors.ts
@@ -0,0 +1,19 @@
+export const BACKGROUND_COLORS = [
+    "rgba(255,49,85)",
+    "rgba(73,247,112)",
+    "rgba(45,174,253)",
+    "rgba(255,237,94)",
+    "rgba(8, 232, 222)",
+    "rgba(142,60,203)",
+    "rgba(255, 0, 127)"
+];
+
+export const BORDER_COLORS = [
+    "rgba(255,49,85)",
+    "rgba(73,247,112)",
+    "rgba(45,174,253)",
+    "rgba(255,237,94)",
+    "rgba(8, 232, 222)",
+    "rgba(142,60,203)",
+    "rgba(255, 0, 127)"
+];
